1202f8af38f852ff0a1947307cfd104184c80a6a
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-29 16:32:18 -0600

    Create variables.tf
    extracted main.tf into modules: subnet & webserver

---
+++ terraform/feature/modules/variables.tf
@@ -0,0 +1,8 @@
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
+variable image_name {}
