52eba565ed6a01e74504dd9a1f481231ea9ebe42
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-29 10:26:06 -0600

    Update days-to-hours.py
    Code cleanup + logical functions
    
    Separated calculation into days_to_units function and all validation into the validate_and_execute function

--- python/days-to-units/days-to-hours.py
+++ python/days-to-units/days-to-hours.py
@@ -3,19 +3,20 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0:
-        return "you entered a 0, please enter a valid positive number"
+
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
     else:
-        return "you entered a negative, so no conversion for you!"
+        print("your input is not a valid number.Don't ruin my program.")
 
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-
-if user_input.isdigit():
-    user_input_number = int(user_input)
-    calculated_value = days_to_units(user_input_number)
-    print(calculated_value)
-else:
-    print("your input is not a number.Don't ruin my program.")
+validate_and_execute()
