980c9b2d88dc5b4d0ba8ca9e1e44ee5ceaa4f55f
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-29 15:27:46 -0600

    Create sets.py
    Understanding Sets

---
+++ python/sets.py
@@ -0,0 +1,20 @@
+# Set Variable
+my_set = {"January", "February", "March"}
+
+for element in my_set:
+    print(element)
+
+# Add Data to Set
+my_set.add("April")
+print(my_set)
+
+# Remove Data from a Set
+my_set.remove("January")
+print(my_set)
+
+# List Variable
+my_list = ["January", "February", "March"]
+
+# Remove Data from a List
+my_list.remove("January")
+print(my_list)
