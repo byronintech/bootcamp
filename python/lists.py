3bb24fd145ed7ce863f1ad31268a891ff3da90d1
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-29 15:04:55 -0600

    Create lists.py
    Understanding how lists work

---
+++ python/lists.py
@@ -0,0 +1,17 @@
+# List Variable
+my_list = ["January", "February", "March"]
+
+# Print Enter List
+print(my_list)
+
+# Print 3rd Item of List
+print(my_list[2])
+
+# Append Item to the List
+my_list.append("April")
+
+# Print 4th Item of the List
+print(my_list[3])
+
+# Print 5th Item of the List but we get an "out of range" error because there is no 5th item
+print(my_list[4])
