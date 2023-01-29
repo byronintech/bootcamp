2b315ff74216ff9e6165c1fd2e50d7da68cdfa4e
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-29 14:18:53 -0600

    Update days-to-hours.py
    Added Try & Except along with negative number error handling.

--- python/days-to-units/days-to-hours.py
+++ python/days-to-units/days-to-hours.py
@@ -3,18 +3,21 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
 
 def validate_and_execute():
-    if user_input.isdigit():
+    try:
+
         user_input_number = int(user_input)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
             print("you entered a 0, please enter a valid positive number")
-    else:
+        else:
+            print("you entered a negative number, no conversion for you!")
+    except ValueError:
         print("your input is not a valid number.Don't ruin my program.")
 
 
