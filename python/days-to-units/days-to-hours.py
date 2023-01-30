ab3f495f5441828882b9a3b3298b39408885ef2b
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-30 07:47:38 -0600

    Update days-to-hours.py
    Added while loop to allow the program to keep running and also some example input.

--- python/days-to-units/days-to-hours.py
+++ python/days-to-units/days-to-hours.py
@@ -8,18 +8,27 @@ def days_to_units(num_of_days):
 
 def validate_and_execute():
     try:
+        user_input_number = int(num_of_days_element)
 
-        user_input_number = int(user_input)
+        # Conversion for positive integers only
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
-            print("you entered a 0, please enter a valid positive number")
+            print("Error: You entered a 0, please enter a valid positive number")
         else:
-            print("you entered a negative number, no conversion for you!")
+            print("Error: You entered a negative number, no conversion for you!")
     except ValueError:
-        print("your input is not a valid number.Don't ruin my program.")
+        print("Error: Your input is not a valid number.Don't ruin my program.")
 
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-validate_and_execute()
+user_input = ""
+username = input("Hey, what's your name?\n")
+
+while user_input != "exit":
+    print('-' * 60)
+    user_input = input(f"Hey {username}, please enter a number of days and I will convert it to hours!\n"
+                       "(Example: 1, 2, 3)\n")
+    list_of_days = user_input.split(", ")
+    for num_of_days_element in set(list_of_days):
+        validate_and_execute()
