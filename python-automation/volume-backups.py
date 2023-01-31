8607ecbe49fc34af81c801b6abb5d3a7b6095056
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-01-31 08:20:42 -0600

    Added Snapshot ID
    Added Snapshot ID and removed excess print statements

--- python-automation/volume-backups.py
+++ python-automation/volume-backups.py
@@ -6,19 +6,15 @@ ec2_client = boto3.client('ec2', region_name="us-west-2")
 
 def create_volume_snapshots():
     volumes = ec2_client.describe_volumes()
-    print("-----" * 15)
-    print("EC2 Volume Dictionary:")
-    print(volumes['Volumes'])
-    print("-----" * 15)
 
     for volume in volumes['Volumes']:
-        # print("-----" * 15)
         ec2_vol_id = volume['VolumeId']
         print(f"EC2 Volume ID: {ec2_vol_id}")
         new_snapshot = ec2_client.create_snapshot(
             VolumeId=volume['VolumeId']
         )
-        print(f"Snapshot Info: {new_snapshot}")
+        # print(f"Snapshot Info: {new_snapshot}")
+        print(f"Snapshot ID: {new_snapshot['SnapshotId']}")
         print("-----" * 15)
 
 
