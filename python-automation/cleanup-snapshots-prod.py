0593b84a71979ded481196e2b43eb9698219f624
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-02-01 05:00:53 -0600

    prod snapshot cleanup

---
+++ python-automation/cleanup-snapshots-prod.py
@@ -0,0 +1,44 @@
+import boto3
+from operator import itemgetter
+import schedule
+
+ec2_client = boto3.client('ec2', region_name="us-west-2")
+
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': ['prod']
+        }
+    ]
+)
+
+
+def delete_snapshots():
+    for volume in volumes['Volumes']:
+        snapshots = ec2_client.describe_snapshots(
+            OwnerIds=['self'],
+            Filters=[
+                {
+                    'Name': 'volume-id',
+                    'Values': [volume['VolumeId']]
+                }
+            ]
+        )
+
+        sorted_by_date = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
+
+        for snap in sorted_by_date[2:]:
+            response = ec2_client.delete_snapshot(
+                SnapshotId=snap['SnapshotId']
+            )
+            print(response)
+
+
+delete_snapshots()
+
+# Schedule to Run Every Week
+"""schedule.every().saturday.do(delete_snapshots)
+
+while True:
+    schedule.run_pending()"""
