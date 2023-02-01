1ac31689ac7111827225cb284532dc4f5642c83d
Byron Smith <50594153+byronksmith@users.noreply.github.com>
2023-02-01 07:01:44 -0600

    Create restore-volume.py

---
+++ python-automation/restore-volume.py
@@ -0,0 +1,57 @@
+import boto3
+from operator import itemgetter
+
+ec2_client = boto3.client('ec2', region_name="us-west-2")
+ec2_resource = boto3.resource('ec2', region_name="us-west-2")
+
+instance_id = "i-0773070ce6ae31527"
+
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'attachment.instance-id',
+            'Values': [instance_id]
+        }
+    ]
+)
+
+instance_volume = volumes['Volumes'][0]
+
+snapshots = ec2_client.describe_snapshots(
+    OwnerIds=['self'],
+    Filters=[
+        {
+            'Name': 'volume-id',
+            'Values': [instance_volume['VolumeId']]
+        }
+    ]
+)
+
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+print(latest_snapshot['StartTime'])
+
+new_volume = ec2_client.create_volume(
+    SnapshotId=latest_snapshot['SnapshotId'],
+    AvailabilityZone='us-west-2b',
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'prod'
+                }
+            ]
+        }
+    ]
+)
+
+while True:
+    vol = ec2_resource.Volume(new_volume['VolumeId'])
+    print(vol.state)
+    if vol.state == 'available':
+        ec2_resource.Instance(instance_id).attach_volume(
+            Device='/dev/xvdb',
+            VolumeId=new_volume['VolumeId']
+        )
+        break
